9c6e92f00b2ba22e0d9ad88779965279ac6f95b4
Kevin Chu <kevin@velexi.com>
2022-02-27 20:05:15 -0800

    Polish docstrings.

--- src/jltest/jltest.jl
+++ src/jltest/jltest.jl
@@ -1,13 +1,6 @@
 """
 The `jltest` module provides support for running unit tests.
 
-Acknowledgements
-----------------
-* Much of the core functionality of the `TestSetPlus` type (and associated methods) were
-  taken directly from the TestSetExtensions package developed by Spencer Russell and
-  his collaborators (https://github.com/ssfrr/TestSetExtensions.jl). `TestSetPlus` builds
-  upon the foundation of `ExtendedTestSet`
-
 -------------------------------------------------------------------------------------------
 COPYRIGHT/LICENSE. This file is part of the TestTools.jl package. It is subject to the
 license terms in the LICENSE file found in the root directory of this distribution. No
