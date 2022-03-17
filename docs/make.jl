872f5979fdbb9955c8d50c74e20aa8e9376b4abc
Kevin Chu <kevin@velexi.com>
2022-03-16 21:25:19 -0700

    Prepare package documentation.

---
+++ docs/make.jl
@@ -0,0 +1,39 @@
+# --- Imports
+
+# Standard library
+using Documenter
+
+# Local package
+using TestTools
+
+# --- Setup
+
+# Make sure that the Julia source code directory is on LOAD_PATH
+push!(LOAD_PATH, "../src/")
+
+# Set up DocMeta
+DocMeta.setdocmeta!(TestTools, :DocTestSetup, :(using TestTools); recursive=true)
+
+# --- Generate documentation
+
+makedocs(;
+    modules=[TestTools],
+    authors="Kevin Chu <kevin@velexi.com> and contributors",
+    repo="https://github.com/velexi-corporation/TestTools.jl/blob/{commit}{path}#{line}",
+    sitename="TestTools.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://ktchu.github.io/TestPkg.jl",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+        # "jltest" => "jltest.md",
+        # "jlcoverage" => "jlcoverage.md",
+        # "jlcodestyle" => "jlcodestyle.md",
+    ],
+)
+
+# --- Deploy documentation
+
+deploydocs(; repo="github.com/velexi-corporation/TestTools.jl", devbranch="main")
