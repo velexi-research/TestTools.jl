6d01798d57a57d5e8fec739df898e12136dcdc20
Kevin Chu <kevin@velexi.com>
2022-03-19 15:36:12 -0700

    Fix bugs in make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -19,7 +19,7 @@ makedocs(;
     sitename="TestTools.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://juliadocs.github.io/TestTools.jl/stable",
+        canonical="https://velexi-corporation.github.io/TestTools.jl/stable",
         assets=String[],
     ),
     pages=[
